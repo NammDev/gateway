6e358a8a21c782ea3a4bc4aa1928feaac1928345
Nam <namdeveloper.ca@gmail.com>
2023-11-09 20:25:44 +0700

    Do it again

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
